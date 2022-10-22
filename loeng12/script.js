57197af5a7910de82471980064c99bf6f4c26a3c
aleksei.komarov <aleksei.komarov@icloud.com>
2022-10-22 10:47:37 +0300

    kalkulaator mis on tunnis tehtud

--- loeng12/script.js
+++ loeng12/script.js
@@ -1,18 +1,22 @@
-const vastuseElement = document.querySelector('.vastus');
+let vastuseElement = document.querySelector('.vastus');
 
 let liidetavad = [];
 let viimatiVajutatiOperaatorit = false;
 
-const vajutatiNuppu = (nupp) => {
+const vajutatiNuppu = async (nupp) => {
     if(typeof nupp === 'number'){
         if(vastuseElement.innerHTML === '0' || viimatiVajutatiOperaatorit){
             vastuseElement.innerHTML = nupp;
         } else {
             vastuseElement.innerHTML += nupp;
         }
+        viimatiVajutatiOperaatorit = false;
     }
+
     if(nupp === 'AC'){
         vastuseElement.innerHTML = 0;
+        liidetavad = [];
+        viimatiVajutatiOperaatorit = false;
     }
     if(nupp === '+'){
         liidetavad.push(vastuseElement.innerHTML);
@@ -21,17 +25,45 @@ const vajutatiNuppu = (nupp) => {
     if(nupp === '='){
         liidetavad.push(vastuseElement.innerHTML);
         viimatiVajutatiOperaatorit = true;
+        console.log('saadame vastuse');
+        const vastusJson = await fetch('http://localhost:3000/liida', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(liidetavad)
+        })
+        const vastus = await vastusJson.json();
+        vastuseElement.innerHTML = vastus.vastus;
     }
 
 
 }
 
-
-
 document.addEventListener("keypress", function onEvent(event){
-    if(event.key === '=') vajutatiNuppu('=');
-    return
-    if(event.key === '*') vajutatiNuppu('=');
-
-
+   if(event.key === '=') {
+        vajutatiNuppu('=');
+        return;
+   }
+   if(event.key === '*'){
+        vajutatiNuppu('x');
+        return;
+   }
+   if(!isNaN(event.key)){
+        vajutatiNuppu(parseInt(event.key));
+        return;
+   }
+   console.log(event.key);
+   console.log(typeof event.key);
+   vajutatiNuppu(event.key);
 });
+
+document.onkeydown = function(evt){
+    evt = evt || window.event;
+    var isEscape = false;
+    if("key" in evt){
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    }
+    if(isEscape){
+        vajutatiNuppu('AC')
+    }
+};
